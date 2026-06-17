0ad9aab5d9b48f5dfcd5c79cd8abf5dfa83df379
Rafael da Conceição <mnraffaell@gmail.com>
2026-06-17 17:31:29 -0300

    Refatora estrutura base do layout

---
+++ Inventto.ui/src/app/layouts/system/constants/route-labels.ts
@@ -0,0 +1,9 @@
+export const ROUTE_LABELS: Record<string, string> = {
+  '/': 'Início',
+  '/products': 'Produtos',
+  '/products/create': 'Novo Produto',
+  '/movements': 'Movimentação',
+  '/movements/new': 'Nova Movimentação',
+  '/team': 'Equipe',
+  '/settings': 'Configurações'
+};
